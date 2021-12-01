2a639cfa2c788ded9f1131e3edb1f762fc533091
MarcoSpicuzza00 <marcospicuzza00@gmail.com>
2021-12-01 20:17:41 +0100

    First version of Etch A Sketch

---
+++ app.js
@@ -0,0 +1,19 @@
+const gridContainer = document.querySelector(".gridContainer");
+const columns = 16;
+const rows = 16;
+
+function createGrid(){
+    for(let i = 0; i < (columns * rows); i++){
+        const square = document.createElement("div");
+        gridContainer.appendChild(square);
+
+        gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        // giving class name so can apply css
+        square.className = "square";
+        square.addEventListener("mouseover", function(){
+            square.style.background = "black";
+        });
+    }
+}
+createGrid();
